d44861396423a55d826b65fecb479f0411610b68
Kumar Vishal <75200239+iamkumarvishal@users.noreply.github.com>
2020-12-03 11:29:08 +0530

    Don't use this file in Anywhere...

---
+++ main.py
@@ -0,0 +1,15 @@
+import PIL
+from PIL import Image
+from tkinter.filedialog import *
+
+file_path = askopenfilename()
+img = PIL.Image.open(file_path)
+myHeight, myWidth = img.size
+
+img = img.resize((myHeight, myWidth), PIL.Image.ANTIALIAS)
+save_path = askopenfilename()
+
+img.save(save_path+"_compressed.JPG")
+
+
+
